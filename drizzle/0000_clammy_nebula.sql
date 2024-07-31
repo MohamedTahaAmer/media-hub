0e98b1909e4dcafb84bacf1a52aa1c29fe8dea7c
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-07-31 15:41:27 +0300

    fix: :bug: Untrak the Drizzle migration files for now

--- drizzle/0000_clammy_nebula.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `thumbnails` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`key` text,
-	`stats` text
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `thumbnails_key_unique` ON `thumbnails` (`key`);
