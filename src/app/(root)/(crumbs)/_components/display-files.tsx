a0d8b6fc104f4cc0d50d76bf5fabe8303ce3adca
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-08-06 11:44:37 +0300

    style: :art: updates to the DisplayFiles component, moving from condinced grid to a max 4 column grid

--- src/app/(root)/(crumbs)/_components/display-files.tsx
+++ src/app/(root)/(crumbs)/_components/display-files.tsx
@@ -8,36 +8,34 @@ import path from "path"
 
 const DisplayFiles = ({ filesAndFolders, directoryName = "" }: { filesAndFolders: FilesAndFolders; directoryName?: string }) => {
 	return (
-		<div className="grid grid-cols-8 gap-2 max-xl:grid-cols-7 max-lg:grid-cols-5 max-md:grid-cols-4 max-sm:grid-cols-3">
+		<div className="grid grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
 			{filesAndFolders.map((item) => (
-				<div key={item.name} className="relative">
-					<div className="truncate rounded-md bg-slate-200 pb-2 dark:bg-slate-700">
-						{/* Thumbnails and Icons */}
-						{item.thumbnail && <Image src={item.thumbnail} alt={`thumbnail for video ${item.name}`} className="" width={320} height={240} />}
-						{!item.thumbnail && (
-							<Link href={`/dir/${encodeURIComponent(path.join(directoryName, item.name))}`}>
-								<DisplayIcon
-									name={item.name}
-									isDirectory={item.isDirectory}
-									className={cn("px-[12.5%]", {
-										"py-3": item.isDirectory,
-									})}
-								/>
+				<div key={item.name} className="overflow-hidden rounded-md pb-2 shadow-xl shadow-slate-400 dark:shadow-slate-700">
+					{/* Thumbnails and Icons */}
+					{item.thumbnail && <Image src={item.thumbnail} alt={`thumbnail for video ${item.name}`} className="" width={320} height={240} />}
+					{!item.thumbnail && (
+						<Link href={`/dir/${encodeURIComponent(path.join(directoryName, item.name))}`}>
+							<DisplayIcon
+								name={item.name}
+								isDirectory={item.isDirectory}
+								className={cn("px-[12.5%]", {
+									"py-3": item.isDirectory,
+								})}
+							/>
+						</Link>
+					)}
+
+					<DisplayFileText isDirectory={item.isDirectory} name={item.name} dir={encodeURIComponent(path.join(directoryName, item.name))} />
+
+					{/* size and download button for files */}
+					{!item.isDirectory && (
+						<div className="flex justify-between gap-2 px-2">
+							<Link href={`/api/download/${encodeURIComponent(path.join(directoryName, item.name))}`}>
+								<DownloadIcon />
 							</Link>
-						)}
-
-						<DisplayFileText isDirectory={item.isDirectory} name={item.name} dir={encodeURIComponent(path.join(directoryName, item.name))} />
-
-						{/* size and download button for files */}
-						{!item.isDirectory && (
-							<div className="flex justify-between gap-2 px-2">
-								<Link href={`/api/download/${encodeURIComponent(path.join(directoryName, item.name))}`}>
-									<DownloadIcon />
-								</Link>
-								<div className="">{item.size}</div>
-							</div>
-						)}
-					</div>
+							<div className="">{item.size}</div>
+						</div>
+					)}
 				</div>
 			))}
 		</div>
@@ -49,9 +47,11 @@ export default DisplayFiles
 function DisplayFileText({ isDirectory, name, dir }: { isDirectory: boolean; name: string; dir: string }) {
 	let baseEndPoint = isDirectory ? "/dir/" : "/api/download/"
 	return (
-		<Link href={`${baseEndPoint}${dir}`} className="pl-2 font-bold">
-			{name}
-		</Link>
+		<div className="truncate-two-lines pl-2">
+			<Link href={`${baseEndPoint}${dir}`} className="font-bold">
+				{name}
+			</Link>
+		</div>
 	)
 }
 
