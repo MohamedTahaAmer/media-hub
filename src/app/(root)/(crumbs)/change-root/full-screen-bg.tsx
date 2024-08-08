9d1448e948e04382270d6aeeb961fb0a897f4855
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-08-09 01:12:54 +0300

    style: :art: Fix the background of the change-dir page to accommodate for the breadcrumbs nav

--- src/app/(root)/(crumbs)/change-root/full-screen-bg.tsx
+++ src/app/(root)/(crumbs)/change-root/full-screen-bg.tsx
@@ -3,7 +3,7 @@ import MediaHub from "@/components/svgs/media-hub"
 const FullScreenBg = () => {
 	return (
 		<>
-			<div className="fixed inset-0 flex items-center justify-center pt-16 blur-xl">
+			<div className="fixed inset-0 flex items-center justify-center pt-40 blur-xl">
 				<MediaHub
 					style={{
 						height: "calc(100vh - 4rem)",
