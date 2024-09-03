dc76e57a6f7c136dc7673c3e6b182a2a84a0ab74
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2024-09-03 18:03:01 +0300

    feat: :fire: Allow server actions to be called from other origins

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
 	images: {
 		domains: ["192.168.1.106"],
 	},
+	experimental: {
+		serverActions: {
+			allowedOrigins: ["192.168.1.106:3002"],
+		},
+	},
 }
 
 export default nextConfig
